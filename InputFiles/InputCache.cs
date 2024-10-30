f595023f404b87f44068597ab86639d59ecd2b1a
Rich Reeves <rawsharklives@gmail.com>
2024-10-30 21:06:02 +0000

    feat: Initial commit of boiler plate in .NET 9

---
+++ InputFiles/InputCache.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace AdventOfCode.InputFiles
+{
+    public static class InputCache
+    {
+        public static string[] Get(int day, bool useTestFile = false)
+        {
+            // Input Files themselves are ignored in GIT
+            return File.ReadAllLines(Program.GetDayFilePath(day, useTestFile));
+        }
+    }
+}
---
+++ Solutions/DayNN.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.InputFiles;
+
+namespace AdventOfCode.Solutions
+{
+    public static class DayNN
+    {
+        private const int Day = 99;
+
+        public static class Part1
+        {
+            public static double GetAnswer()
+            {
+                string[] inputData = InputCache.Get(Day, true);
+                return Shared.Result(inputData);
+            }
+        }
+
+        public static class Part2
+        {
+            public static double GetAnswer()
+            {
+                string[] inputData = InputCache.Get(Day, true);
+                return Shared.Result(inputData, true);
+            }
+        }
+
+        private class Shared
+        {
+            public static double Result(string[] set, bool partTwo = false)
+            {
+                var result = 0d;
+                for (int i = 0; i < set.Length; i++)
+                {
+                    if (set[i].Trim() == string.Empty)
+                    {
+                        continue;
+                    }
+                    result += 1;
+                }
+                return -1;
+            }
+        }
+    }
+}
